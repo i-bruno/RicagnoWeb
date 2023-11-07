3eecb027721d9e407fbfa35682dbb688178932dd
Ignacio Bruno <ignaciobruno90@gmail.com>
2023-11-06 22:16:03 -0300

    Se trabajaron en los estilos de la página

--- src/components/Construccion/Construccion.jsx
+++ src/components/Construccion/Construccion.jsx
@@ -4,14 +4,13 @@ import Redes from '../Redes/Redes'
 
 const Construccion = () => {
     return (
-        <>
             <div className='mensaje'>
+                <img src=".\img\obras\07_inestable\inestableDetalle2.jpg" alt="obra portada" className='imgObra'/>
                 <h1 className='construccion'>
                     PÁGINA EN CONSTRUCCIÓN
                 </h1>
                 <Redes />
             </div>
-        </>
     )
 }
 

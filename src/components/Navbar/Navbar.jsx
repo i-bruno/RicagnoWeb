4d187aa8ff139ff0d8f52ba6c48eda2e12cfbcfd
Ignacio Bruno <ignaciobruno90@gmail.com>
2023-11-08 12:56:05 -0300

    Se solucionó el problema del colapsable del navbar a partir de uso de estados de react

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import "./Navbar.css"
+import { useState } from 'react';
+
 
 export const Navbar = () => {
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
 
     return (
         <>
@@ -12,18 +24,26 @@ export const Navbar = () => {
 
                         <a class="navbar-brand" href="/">Rocío Ricagno</a>
 
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button className={`navbar-toggler ${isMenuOpen ? '' : 'collapsed'}`}
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                            onClick={toggleMenu}
+                        >
 
                             <span className="navbar-toggler-icon"></span>
 
                         </button>
 
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-                                <li className="nav-item">
+                                <li className="nav-item" onClick={closeMenu}>
                                     <NavLink to={`/`} className="nav-link" aria-current="page">Inicio</NavLink>
                                 </li>
-                                <li className="nav-item">
+                                <li className="nav-item" onClick={closeMenu}>
                                     <NavLink to={`/bio`} className="nav-link">Bio</NavLink>
                                 </li>
                                 <li className="nav-item dropdown">
@@ -31,60 +51,60 @@ export const Navbar = () => {
                                         Artworks
                                     </a>
                                     <ul className="dropdown-menu">
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/proyectoarana`} className="dropdown-item w-auto">
                                                 Proyecto Arana
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/silencio`} className="dropdown-item w-auto">
                                                 Hacer hablar al silencio
                                             </NavLink>
 
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/capasabrigo`} className="dropdown-item w-auto">
                                                 Capas de abrigo
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/pielderio`} className="dropdown-item w-auto">
                                                 Piel de río
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/sintesis`} className="dropdown-item w-auto">
                                                 Sintesis de la memoria
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/memoria`} className="dropdown-item w-auto">
                                                 Memoria cartográfica
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/inestable`} className="dropdown-item w-auto">
                                                 Inestable
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/devastacion`} className="dropdown-item w-auto">
                                                 Devastación
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/devenirterritorio`} className="dropdown-item w-auto">
                                                 Devenir territorio
                                             </NavLink>
                                         </li>
-                                        <li>
+                                        <li onClick={closeMenu}>
                                             <NavLink to={`/seniorita`} className="dropdown-item w-auto">
                                                 Señorita
                                             </NavLink>
                                         </li>
                                     </ul>
                                 </li>
-                                <li className="nav-item">
+                                <li className="nav-item" onClick={closeMenu}>
                                     <NavLink to={`/contacto`} className="nav-link">Contacto</NavLink>
                                 </li>
                             </ul>
